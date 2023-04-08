683eb2f3ecf12bccea8c9b4b381b175069f14936
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2023-04-08 23:11:37 +0700

    Create Conspect.py

---
+++ Conspect.py
@@ -0,0 +1,25 @@
+#ЛЕКЦИЯ 1
+print(5,8,6)
+n=5
+print(n)
+q ='qwe\'rty'
+p = "qwerty"
+#print(type(n))
+#print(f"{n} - {q} - {p}")
+#print("{}-{}-{}".format(n,p,q))
+#print('Введите число')
+#a= int(input())
+#print(a)
+#b=int(input('введите 2 число'))
+#print(a ,'+', b ,'=', a + b)
+a = 3.65656
+b = 4.8888888
+print(round(a*b,3))
+n=423
+sum = 0
+while n>0:
+    x = n%10
+    sum=sum+x
+    n=n//10
+    print (sum)
+
